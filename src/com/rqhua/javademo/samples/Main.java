750f546dc145e71b8a5dba1f90d4d098f49cea32
xiaocaifly <rqhua_fly@163.com>
2018-05-24 23:13:28 +0800

    Initial commit

---
+++ src/com/rqhua/javademo/samples/Main.java
@@ -0,0 +1,33 @@
+package com.rqhua.javademo.samples;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "学Java";
+
+        int a =1;
+
+        System.out.println("byte " + Byte.BYTES);
+        System.out.println("short " + Short.BYTES);
+        System.out.println("char " + Character.BYTES);
+        System.out.println("int " + Integer.BYTES);
+        System.out.println("float " + Float.BYTES);
+        System.out.println("double " + Double.BYTES);
+        System.out.println("long " + Long.BYTES);
+
+
+        printLength(s);
+        printLength("学");
+        printLength("華");
+        printLength("J");
+        printLength("Java");
+
+    }
+    private static void printLength(String string) {
+        System.out.println(string + " 所占内存 " + string.getBytes().length + " byte");
+    }
+
+    private static void printBasicTypeBytes(){
+
+    }
+
+}
